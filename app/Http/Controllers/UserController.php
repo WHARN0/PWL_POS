93428eb55985e879db42d2e4d9509527e55dace9
Bagus Arnovario Wibowo <variosrio@gmail.com>
2024-03-07 13:49:28 +0700

    Update Praktikum 2.3 Jobsheet 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -27,7 +27,12 @@ class UserController extends Controller
         // return view('user', ['data' => $user]);
         // $user = UserModel::findOrFail(1);
         // return view('user', ['data' => $user]);
-        $user = UserModel::where('username', 'manager9')->firstOrFail();
+        // $user = UserModel::where('username', 'manager9')->firstOrFail();
+        // return view('user', ['data' => $user]);
+        // $user = UserModel::where('level_id', 2)->count();
+        // dd($user);
+        // return view('user', ['data' => $user]);
+        $user = UserModel::where('level_id', 2)->count();
         return view('user', ['data' => $user]);
     }
 }
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -8,7 +8,24 @@
     <title>Data User</title>
 </head>
 
+
 <body>
+    <h1>Data User</h1>
+    <table border="1" cellpadding="2" cellspacing="0">
+        <tr>
+            <td>Jumlah Pengguna</td>
+        </tr>
+        {{-- @foreach ($data as $d) --}}
+        <tr>
+            <td>{{$data}}</td>
+        </tr>
+        {{-- @endforeach --}}
+    </table>
+</body>
+
+</html>
+
+{{-- <body>
     <h1>Data User</h1>
     <table border="1" cellpadding="2" cellspacing="0">
         <tr>
@@ -17,15 +34,13 @@
             <td>Nama</td>
             <td>ID Level Pengguna</td>
         </tr>
-        {{-- @foreach ($data as $d) --}}
-            <tr>
-                <td>{{ $data->user_id }}</td>
-                <td>{{ $data->username }}</td>
-                <td>{{ $data->nama }}</td>
-                <td>{{ $data->level_id }}</td>
-            </tr>
-        {{-- @endforeach --}}
-    </table>
-</body>
-
-</html>
+        @foreach ($data as $d) --}}
+{{-- <tr>
+    <td>{{ $data->user_id }}</td>
+    <td>{{ $data->username }}</td>
+    <td>{{ $data->nama }}</td>
+    <td>{{ $data->level_id }}</td>
+</tr> --}}
+{{-- @endforeach --}}
+{{-- </table>
+</body> --}}
