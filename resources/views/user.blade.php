d3925d2ecbb38a34e08166b24c2c47083421b1b1
Bagus Arnovario Wibowo <variosrio@gmail.com>
2024-03-07 19:52:22 +0700

    Update Praktikum 2.6 dan 2.7 Jobsheet 4

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -7,10 +7,12 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Data User</title>
 </head>
+
 <body>
     <h1>Data User</h1>
+    <a href="{{ url('/user/tambah') }}">+ Tambah User</a>
     <table border="1" cellpadding="2" cellspacing="0">
-        <tr>
+        {{-- <tr>
             <td>ID</td>
             <td>Username</td>
             <td>Nama</td>
@@ -21,21 +23,64 @@
             <td>{{ $data->username }}</td>
             <td>{{ $data->nama }}</td>
             <td>{{ $data->level_id }}</td>
+        </tr> --}}
+
+        {{-- <tr>
+            <td>ID</td>
+            <td>Username</td>
+            <td>Nama</td>
+            <td>ID Level Pengguna</td>
+            <td>Aksi</td>
         </tr>
+        @foreach ($data as $d)
+            <tr>
+                <td>{{ $d->user_id }}</td>
+                <td>{{ $d->username }}</td>
+                <td>{{ $d->nama }}</td>
+                <td>{{ $d->level_id }}</td>
+                <td><a href="{{ url('/user/ubah/' . $d->user_id)}}">Ubah</a> <a
+                        href="{{ url('/user/hapus/' . $d->user_id)}}">Hapus</a>
+                </td>
+            </tr>
+        @endforeach --}}
+
+        <tr>
+            <td>ID</td>
+            <td>Username</td>
+            <td>Nama</td>
+            <td>ID Level Pengguna</td>
+            <td>Kode Level</td>
+            <td>Nama Level</td>
+            <td>Aksi</td>
+        </tr>
+        @foreach ($data as $d)
+            <tr>
+                <td>{{ $d->user_id }}</td>
+                <td>{{ $d->username }}</td>
+                <td>{{ $d->nama }}</td>
+                <td>{{ $d->level_id }}</td>
+                <td>{{ $d->level->level_kode }}</td>
+                <td>{{ $d->level->level_nama }}</td>
+                <td><a href="{{ url('/user/ubah/' . $d->user_id) }}">Ubah</a> <a
+                        href="{{ url('/user/hapus/' . $d->user_id) }}">Hapus</a>
+                </td>
+            </tr>
+        @endforeach
     </table>
 </body>
+
 </html>
 
 {{-- <body>
     <h1>Data User</h1> --}}
-    {{-- <table border="1" cellpadding="2" cellspacing="0">
+{{-- <table border="1" cellpadding="2" cellspacing="0">
         <tr>
             <td>Jumlah Pengguna</td>
         </tr> --}}
-        {{-- @foreach ($data as $d) --}}
-        {{-- <tr>
+{{-- @foreach ($data as $d) --}}
+{{-- <tr>
             <td>{{$data}}</td>
         </tr> --}}
-        {{-- @endforeach --}}
-    {{-- </table>
+{{-- @endforeach --}}
+{{-- </table>
 </body> --}}
